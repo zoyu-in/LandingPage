22671a3b2df6efb1ba19053fe1b44c0737ff7169
Saurabh Singh <saurabhsinghchahar192@gmail.com>
2020-06-17 20:06:39 +0530

    initial

---
+++ src/components/index.vue
@@ -0,0 +1,18 @@
+<template>
+     <v-container>
+         <v-row>
+             <v-col cols="6"> hello</v-col>
+             <v-col cols="6">there</v-col>
+         </v-row>
+     </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
